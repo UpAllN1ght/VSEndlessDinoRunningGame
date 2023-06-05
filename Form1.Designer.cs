43988a85eee29869bd515cea5e4e76cb308f2c15
Alexandra D <116899121+UpAllN1ght@users.noreply.github.com>
2023-06-05 16:45:47 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace T_Rex_Endless_Runner_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.obstacle1 = new System.Windows.Forms.PictureBox();
+            this.dino = new System.Windows.Forms.PictureBox();
+            this.floor = new System.Windows.Forms.PictureBox();
+            this.background = new System.Windows.Forms.PictureBox();
+            this.obstacle2 = new System.Windows.Forms.PictureBox();
+            this.scoreText = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dino)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.floor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.background)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // obstacle1
+            // 
+            this.obstacle1.BackColor = System.Drawing.Color.Transparent;
+            this.obstacle1.Image = global::Dino_Endless_Running_Game.Properties.Resources.obstacle11;
+            this.obstacle1.Location = new System.Drawing.Point(788, 358);
+            this.obstacle1.Name = "obstacle1";
+            this.obstacle1.Size = new System.Drawing.Size(82, 72);
+            this.obstacle1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.obstacle1.TabIndex = 3;
+            this.obstacle1.TabStop = false;
+            this.obstacle1.Tag = "obstacle";
+            // 
+            // dino
+            // 
+            this.dino.BackColor = System.Drawing.Color.Transparent;
+            this.dino.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.dino.Image = global::Dino_Endless_Running_Game.Properties.Resources.dino;
+            this.dino.Location = new System.Drawing.Point(26, 245);
+            this.dino.Name = "dino";
+            this.dino.Size = new System.Drawing.Size(249, 152);
+            this.dino.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dino.TabIndex = 2;
+            this.dino.TabStop = false;
+            // 
+            // floor
+            // 
+            this.floor.BackColor = System.Drawing.Color.Black;
+            this.floor.Location = new System.Drawing.Point(-6, 386);
+            this.floor.Name = "floor";
+            this.floor.Size = new System.Drawing.Size(897, 84);
+            this.floor.TabIndex = 1;
+            this.floor.TabStop = false;
+            // 
+            // background
+            // 
+            this.background.BackColor = System.Drawing.Color.Transparent;
+            this.background.Image = ((System.Drawing.Image)(resources.GetObject("background.Image")));
+            this.background.Location = new System.Drawing.Point(0, 0);
+            this.background.Name = "background";
+            this.background.Size = new System.Drawing.Size(891, 471);
+            this.background.TabIndex = 0;
+            this.background.TabStop = false;
+            // 
+            // obstacle2
+            // 
+            this.obstacle2.BackColor = System.Drawing.Color.Transparent;
+            this.obstacle2.Image = global::Dino_Endless_Running_Game.Properties.Resources.obstacle21;
+            this.obstacle2.Location = new System.Drawing.Point(573, 328);
+            this.obstacle2.Name = "obstacle2";
+            this.obstacle2.Size = new System.Drawing.Size(74, 102);
+            this.obstacle2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.obstacle2.TabIndex = 4;
+            this.obstacle2.TabStop = false;
+            this.obstacle2.Tag = "obstacle";
+            // 
+            // scoreText
+            // 
+            this.scoreText.AutoSize = true;
+            this.scoreText.BackColor = System.Drawing.Color.IndianRed;
+            this.scoreText.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreText.Location = new System.Drawing.Point(21, 17);
+            this.scoreText.Name = "scoreText";
+            this.scoreText.Size = new System.Drawing.Size(108, 29);
+            this.scoreText.TabIndex = 5;
+            this.scoreText.Text = "score- 0";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(882, 470);
+            this.Controls.Add(this.scoreText);
+            this.Controls.Add(this.obstacle2);
+            this.Controls.Add(this.obstacle1);
+            this.Controls.Add(this.dino);
+            this.Controls.Add(this.floor);
+            this.Controls.Add(this.background);
+            this.Name = "Form1";
+            this.Text = "Dino Endless Runner";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dino)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.floor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.background)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox floor;
+        private System.Windows.Forms.PictureBox dino;
+        private System.Windows.Forms.PictureBox background;
+        private System.Windows.Forms.PictureBox obstacle1;
+        private System.Windows.Forms.PictureBox obstacle2;
+        private System.Windows.Forms.Label scoreText;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using T_Rex_Endless_Runner_Game;
+
+namespace Dino_Endless_Running_Game
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
